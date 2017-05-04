c316b35a83d34445d7105313958264acfede418e
buckeye76guy <hounyo.1@osu.edu>
2017-05-04 17:25:20 -0400

    Proceesing and Running model
    The data_processing.py file is for the gaussian filter. The others have
    resize appended to the name because these ones are the ones where the
    images were resized. The run_models.py is the one used for the mixture
    of alphabets. The other two have lat for Latin and cyr for Cyrillic
    appended to the names.

---
+++ data_processing_resize_lat.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 17:05:44 2017
+
+@author: Josiah Hounyo
+
+This script will create the training and testing sets.
+I wish to use 75% of the number of pictures in each folder
+for training and 25% for testing
+"""
+
+from scipy.misc import imresize
+import scipy.ndimage as nd
+import numpy as np
+import os
+
+# current directory has two folders
+flders = ['Latin']
+
+# get the number of observations to be placed in the train
+# and test set
+nrow_tr, nrow_te = 0, 0
+for fld in flders:
+    os.chdir(fld)
+    # inside each directory is a list of directories
+    subflders = os.listdir() # subfolders
+    for subfld in subflders:
+        nrow = len(os.listdir(subfld)) # number of photos in current subfolder
+        add_tr = int(np.floor(75*nrow/100)) # to be added to nrow_tr
+        nrow_tr += add_tr
+        nrow_te += nrow - add_tr
+    os.chdir('..') # get back to parent
+
+x_train = np.zeros((nrow_tr, 28, 28)) # I already know size of images
+# y_train = np.zeros((nrow_tr, 1))
+y_train = [] # because they are characters! use int mapping for each :)
+x_test = np.zeros((nrow_te, 28, 28))
+# y_test = np.zeros((nrow_te, 1))
+y_test = []
+
+# it will be assumed that all photos have the same shape.
+# instead of using gaussian filter, I will resize to 28 by 28
+ind_tr, ind_te = 0, 0 # cursor to keep track of location in arrays
+for fld in flders:
+    os.chdir(fld)
+    subflders = os.listdir()
+    for subfld in subflders:
+        nrow = len(os.listdir(subfld))
+        add_tr = int(np.floor(75*nrow/100))
+        # y_train/test is the name of the folder
+        os.chdir(subfld)
+        k = 0 # number of items to add to train set
+        pics = os.listdir() # pictures in folder
+        while k < len(pics):
+            img = nd.imread(pics[k])
+            if sorted(img.shape) != [4, 278, 278]:
+                k += 1 # only this needs be increased
+                # this way, ind_tr and ind_te will be such that only ending
+                # rows in our matrices are all 0's. easily removed
+                continue
+            img = img.reshape((278, 278*4))
+            img = imresize(img, (28,28))
+            if k < add_tr:
+                x_train[ind_tr,:,:] = img
+                y_train.append(subfld) # subfld is a character
+                ind_tr += 1
+            else:
+                x_test[ind_te,:,:] = img
+                y_test.append(subfld)
+                ind_te += 1
+            k += 1
+        os.chdir('..')
+    os.chdir('..')
+
+# process y_train and y_test: this will allow us to save them as integers
+chars = list(set(y_train))
+char_to_ix = {ch: i for i, ch in enumerate(chars)}
+y_train = list(map(lambda x: char_to_ix.get(x), y_train))
+y_test = list(map(lambda x: char_to_ix.get(x), y_test))
+
+np.save('xtrain_lat.npy', x_train)
+np.save('xtest_lat.npy', x_test)
+np.save('ytrain_lat.npy', np.array(y_train))
+np.save('ytest_lat.npy', np.array(y_test))
+
+# some characters in both Latin and Curillic are the same ...
---
+++ run_models_cyr.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 28 20:37:08 2017
+
+@author: Josiah Hounyo
+"""
+
+# may shuffle test set
+from __future__ import print_function
+
+import numpy as np
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, LSTM
+from keras.optimizers import RMSprop
+from keras.utils import np_utils
+from keras import backend as K
+
+K.set_image_dim_ordering('th')
+from theano.tensor.shared_randomstreams import RandomStreams
+from ZRNN import LSTM3, LSTM5
+
+class LossHistory(keras.callbacks.Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.accuracy = []
+
+    def on_epoch_end(self, batch, logs={}):
+        #print(logs)
+        self.losses.append(logs.get('val_loss')) # losses
+        self.accuracy.append(logs.get('val_acc')) # accuracy
+
+batch_size = 32
+nb_epochs = 100 # perhaps 50 epochs is enough
+hidden_units = 100
+
+X_train = np.load('xtrain_cyr.npy')
+max_pix = np.max(X_train)
+X_train /= max_pix
+
+X_test = np.load('xtest_cyr.npy')
+X_test /= max_pix # pretty sure maximum is same accross datasets
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+y_train = np.load('ytrain_cyr.npy')
+y_test = np.load('ytest_cyr.npy')
+
+nb_classes = np.unique(y_train).shape[0]
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+np.random.seed(3)
+srgn = RandomStreams(3)
+
+act = 'tanh'
+lstm = LSTM3
+eta_1 = 5e-4
+
+lstm3 = lstm(consume_less='mem',output_dim=hidden_units,
+                    activation=act,
+                    input_shape=X_train.shape[1:])
+lstm = LSTM5
+lstm5 = lstm(consume_less='mem',output_dim=hidden_units,
+                    activation=act,
+                    input_shape=X_train.shape[1:])
+def baseline_model(eta):
+    model = Sequential()
+    model.add(LSTM(hidden_units, activation=act, 
+                   input_shape = X_train.shape[1:]))
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    rmsprop = RMSprop(lr=eta, rho=0.9, epsilon=1e-8, decay=0)
+    model.compile(loss='categorical_crossentropy',
+              optimizer= 'rmsprop',
+              metrics=['accuracy'])
+    return model
+
+def compare_model(eta, modl):
+    model = Sequential()
+    model.add(modl)
+    model.add(Dense(nb_classes))
+    model.add(Activation('softmax'))
+    rmsprop = RMSprop(lr=eta, rho=0.9, epsilon=1e-8, decay=0)
+    model.compile(loss='categorical_crossentropy',
+              optimizer= 'rmsprop',
+              metrics=['accuracy'])
+    return model
+
+# baseline model with different learning rates
+history = LossHistory()
+########################################################################
+model = baseline_model(eta_1)
+hist = model.fit(X_train, Y_train, batch_size=batch_size, 
+                 nb_epoch=nb_epochs, verbose=1, 
+                 validation_data=(X_test, Y_test), callbacks=[history])
+my_array = np.hstack((np.matrix(history.losses).T,
+                      np.matrix(history.accuracy).T))
+np.save('cyrResults/base_eta1_d.npy', my_array) # d i.e. not from qsub command
+
+eta = 1e-4
+model = baseline_model(eta)
+hist = model.fit(X_train, Y_train, batch_size=batch_size, 
+                 nb_epoch=nb_epochs, verbose=1, 
+                 validation_data=(X_test, Y_test), callbacks=[history])
+my_array = np.hstack((np.matrix(history.losses).T,
+                      np.matrix(history.accuracy).T))
+np.save('cyrResults/base_eta2_d.npy', my_array)
+
+########################################################################
+# LSTM3 with different learning rates
+model = compare_model(eta_1, lstm3)
+hist = model.fit(X_train, Y_train, batch_size=batch_size,
+                 nb_epoch=nb_epochs, verbose=1, 
+                 validation_data=(X_test, Y_test), callbacks=[history])
+my_array = np.hstack((np.matrix(history.losses).T,
+                      np.matrix(history.accuracy).T))
+np.save('cyrResults/lstm3_eta1_d.npy', my_array)
+
+model = compare_model(eta, lstm3)
+hist = model.fit(X_train, Y_train, batch_size=batch_size,
+                 nb_epoch=nb_epochs, verbose=1, 
+                 validation_data=(X_test, Y_test), callbacks=[history])
+my_array = np.hstack((np.matrix(history.losses).T,
+                      np.matrix(history.accuracy).T))
+np.save('cyrResults/lstm3_eta2_d.npy', my_array)
+
+########################################################################
+# LSTM5 with different learning rates
+model = compare_model(eta_1, lstm5)
+hist = model.fit(X_train, Y_train, batch_size=batch_size,
+                 nb_epoch=nb_epochs, verbose=1, 
+                 validation_data=(X_test, Y_test), callbacks=[history])
+my_array = np.hstack((np.matrix(history.losses).T,
+                      np.matrix(history.accuracy).T))
+np.save('cyrResults/lstm5_eta1_d.npy', my_array)
+
+model = compare_model(eta, lstm5)
+hist = model.fit(X_train, Y_train, batch_size=batch_size,
+                 nb_epoch=nb_epochs, verbose=1, 
+                 validation_data=(X_test, Y_test), callbacks=[history])
+my_array = np.hstack((np.matrix(history.losses).T,
+                      np.matrix(history.accuracy).T))
+np.save('cyrResults/lstm5_eta2_d.npy', my_array)
